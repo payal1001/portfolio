919a8cef00007228e2cc7bce80e06fe0a3a89a42
payal1001 <prajapatipayal601@gmail.com>
2023-10-01 16:13:00 +0530

    Initial commit

--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useSyncExternalStore } from 'react'
 import './navbar.css'
 import logo from '../assets/logo-2.png';
 import{ Link } from 'react-scroll';
@@ -7,6 +7,7 @@ import menu from '../assets/menu.png'
 
 
 const Navbar = () => {
+  const [showMenu,setshowMenu] = useState(false);
   return (
     <nav className="navbar">
       <img src={logo} alt= "logo" className="logo" style={{widht:"30%", height:'30%'}}/>
@@ -27,18 +28,18 @@ className="desktopMenuListItem">About</Link>
         <img src={contactImg} alt="" className="desktopMenuImg" /> Contact Me  
       </button>
 
-      <img src={menu} alt= "Menu" className="mobMenu" style={{widht:"30%", height:'30%'}}/>
-      <div className="navMenu">
-        <Link activeClass="active" to="intro" spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Home</Link>
+      <img src={menu} alt= "Menu" className="mobMenu" />
+      <div className="navMenu" style={{display: showMenu? 'flex':'none'}}>
+        <Link activeClass="active" to="intro" spy={true} smooth={true} offset={-100} duration={500} className="ListItem" onlick={ () => setshowMenu(false)}>Home</Link>
 
         <Link activeClass="active" to="skills" spy={true} smooth={true} offset={-100} duration={500} 
-className="desktopMenuListItem">About</Link>
+className="ListItem" onlick={ () => setshowMenu(false)}>About</Link>
 
-        <Link activeClass="active" to="works" spy={true} smooth={true} offset={-100} duration={500}  className="desktopMenuListItem">Portfolio</Link>
+        <Link activeClass="active" to="works" spy={true} smooth={true} offset={-100} duration={500}  className="ListItem" onlick={ () => setshowMenu(false)}>Portfolio</Link>
 
-        <Link activeClass="active" to="Projetcs" spy={true} smooth={true} offset={-100} duration={500}  className="desktopMenuListItem">Projects</Link>
+        <Link activeClass="active" to="Projetcs" spy={true} smooth={true} offset={-100} duration={500}  className="ListItem" onlick={ () => setshowMenu(false)}>Projects</Link>
 
-        <Link activeClass="active" to="contact" spy={true} smooth={true} offset={-100} duration={500}  className="desktopMenuListItem">Contact</Link>
+        <Link activeClass="active" to="contact" spy={true} smooth={true} offset={-100} duration={500}  className="ListItem" onlick={ () => setshowMenu(false)}>Contact</Link>
 
       </div>
 
